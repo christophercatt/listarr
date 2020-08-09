4f32a1d066d72406347401e0b96fa035f2083692
christophercatt <ccatt601@gmail.com>
2020-08-09 22:51:32 +0100

    Bugfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	find . \! -user redis -exec chown listarr '{}' +
+	find . \! -user listarr -exec chown listarr '{}' +
 	exec gosu listarr "$0" "$@"
 fi
 
