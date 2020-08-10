d0b4ed8848cdd413087f9b4468f44d7b372211fb
christophercatt <ccatt601@gmail.com>
2020-08-10 02:15:14 +0100

    Bugfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,6 @@
 
 set -e
 
+chown -R listarr:listarr /app
 
-# store arguments in a special array 
-args=("$@") 
-# get number of elements 
-ELEMENTS=${#args[@]} 
- 
-# echo each element in array  
-# for loop 
-for (( i=0;i<$ELEMENTS;i++)); do 
-    echo ${args[${i}]} 
-done
-
-# allow the container to be started with `--user`
-if [ "$1" = 'npm start' -a "$(id -u)" = '0' ]; then
-	find . \! -user listarr -exec chown listarr '{}' +
-	exec gosu listarr "$0" "$@"
-fi
-
-exec "$@"
+exec gosu listarr "$@"
