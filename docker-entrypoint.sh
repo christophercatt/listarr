43889aecfe490570eb7199fd38cf993ba7f09b9a
christophercatt <ccatt601@gmail.com>
2020-08-10 01:56:49 +0100

    Added ARG Testing Code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,20 @@
 
 set -e
 
+
+# store arguments in a special array 
+args=("$@") 
+# get number of elements 
+ELEMENTS=${#args[@]} 
+ 
+# echo each element in array  
+# for loop 
+for (( i=0;i<$ELEMENTS;i++)); do 
+    echo ${args[${i}]} 
+done
+
 # allow the container to be started with `--user`
-if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+if [ "$1" = 'npm start' -a "$(id -u)" = '0' ]; then
 	find . \! -user listarr -exec chown listarr '{}' +
 	exec gosu listarr "$0" "$@"
 fi
